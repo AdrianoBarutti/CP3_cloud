6996faeedad0b02db671aedf27c1deb8af86f03a
adriano <adrianopessuto@hotmail.com>
2025-09-09 20:10:01 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using MinhaApiOracle.Data;
-using Oracle.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
@@ -10,11 +9,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configuração do banco de dados Azure SQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-
 app.Urls.Add("http://*:8080");
-
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
