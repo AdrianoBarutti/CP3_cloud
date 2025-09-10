eb6645d44cf5fbc56019407a7ceef9c4254ecd41
ntncaue <cauearaujo60@gmail.com>
2025-09-09 22:55:24 -0300

    commit sm30

--- Program.cs
+++ Program.cs
@@ -1,30 +1,29 @@
 using Microsoft.EntityFrameworkCore;
 using MinhaApiOracle.Data;
-using Microsoft.OpenApi.Models;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Conexão com Oracle ou SQL Server
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// UseSqlServer → se estiver usando SQL Server
+// UseOracle → se estiver usando Oracle
+builder.Services.AddDbContext<AppDb>(options =>
+    options.UseSqlServer(connectionString)); // ou UseOracle se for o caso
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configuração do banco de dados Azure SQL
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
 
-app.Urls.Add("http://*:8080");
-
-app.UseSwagger();
-app.UseSwaggerUI(c =>
+if (app.Environment.IsDevelopment())
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API V1");
-});
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.UseAuthorization(); 
-
+app.UseHttpsRedirection();
+app.UseAuthorization();
 app.MapControllers();
-
 app.Run();
